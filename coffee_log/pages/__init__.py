3a97c5c5eac69c3fd5a193ed1c8a4cc89b5af5ad
André Wendler <42637005+a-wendler@users.noreply.github.com>
2024-06-08 13:59:43 +0000

    del reflex

--- coffee_log/pages/__init__.py
+++
@@ -1,5 +0,0 @@
-from .dashboard import dashboard
-from .index import index
-from .settings import settings
-from .registrieren import registrieren
-from .users import users
--- rxconfig.py
+++
@@ -1,6 +0,0 @@
-import reflex as rx
-
-config = rx.Config(
-    app_name="coffee_log",
-    db_url="sqlite:///coffee.db"
-)
