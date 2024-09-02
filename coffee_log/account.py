e7d1add047c291095cb17d3bbfdada8e3f271c6e
André Wendler <42637005+a-wendler@users.noreply.github.com>
2024-09-02 07:29:52 +0000

    finalise accountübersicht

--- coffee_log/account.py
+++ coffee_log/account.py
@@ -12,8 +12,6 @@ def zahlungsliste(conn):
         payments = session.scalars(payments_stmt).all()
 
         zahlungsliste = []
-        sollposten = 0
-        habenposten = 0
         for zahlung in payments:
             if zahlung.typ in ["Auszahlung", "Einkauf"]:
                 zahlungsliste.append(
@@ -25,7 +23,6 @@ def zahlungsliste(conn):
                         "Nutzer": zahlung.user.name,
                     }
                 )
-                sollposten += zahlung.betrag
             else:
                 zahlungsliste.append(
                     {
@@ -36,8 +33,7 @@ def zahlungsliste(conn):
                         "Nutzer": zahlung.user.name,
                     }
                 )
-                habenposten += zahlung.betrag
-        return (zahlungsliste, sollposten, habenposten)
+        return zahlungsliste
 
 
 @st.cache_data(ttl=180)
@@ -63,21 +59,13 @@ def offene_rechnungen(_conn):
 conn = st.connection("coffee_counter", type="sql")
 
 st.title("Kontoübersicht")
-zahlungen, sollposten, habenposten = zahlungsliste(conn)
+zahlungen = zahlungsliste(conn)
 df_zahlungen = pd.DataFrame(zahlungen)
-st.dataframe(
-    df_zahlungen,
-    column_config={
-        "Betrag": st.column_config.NumberColumn(format="€ %g"),
-        "Datum": st.column_config.DatetimeColumn(format="DD.MM.YY"),
-    },
-)
 
-saldo = "€ " + str(df_zahlungen["Betrag"].sum()).replace(".", ",")
-st.metric("Kontostand", saldo)
-
-st.metric("Sollposten", "€ " + str(sollposten).replace(".", ","))
-st.metric("Habenposten", "€ " + str(habenposten).replace(".", ","))
+einzahlungen =  df_zahlungen[df_zahlungen["Typ"] == "Einzahlung"].sum()["Betrag"]
+einkaeufe = df_zahlungen[df_zahlungen["Typ"] == "Einkauf"].sum()["Betrag"]
+auszahlungen = df_zahlungen[df_zahlungen["Typ"] == "Auszahlung"].sum()["Betrag"]
+korrekturen = df_zahlungen[df_zahlungen["Typ"] == "Korrektur"].sum()["Betrag"]
 
 with conn.session as session:
     kaffeeanzahl_mitglieder = session.scalar(
@@ -89,23 +77,8 @@ with conn.session as session:
     mitgliedskosten = kaffeeanzahl_mitglieder * Decimal(st.secrets.KAFFEEPREIS_MITGLIED)
     gastkosten = kaffeeanzahl_gaeste * Decimal(st.secrets.KAFFEEPREIS_GAST)
 
-    st.metric("Kaffeeanzahl Mitglieder", str(kaffeeanzahl_mitglieder))
-    st.metric("Kaffeeanzahl Gäste", str(kaffeeanzahl_gaeste))
-    st.metric("Kaffeeumsatz Mitglieder", "€ " + str(mitgliedskosten).replace(".", ","))
-    st.metric("Kaffeeumsatz Gäste", "€ " + str(gastkosten).replace(".", ","))
-
-st.subheader("offene Rechnungen")
 offene_rechnungen, summe = offene_rechnungen(conn)
-st.dataframe(
-    offene_rechnungen,
-    column_config={
-        "Betrag": st.column_config.NumberColumn(format="€ %g"),
-        "Datum": st.column_config.DatetimeColumn(format="DD.MM.YY"),
-    },
-)
-st.metric("Summe offene Rechnungen", "€ " + str(summe).replace(".", ","))
 
-st.subheader("Saldi der Nutzenden")
 with conn.session as session:
     users = session.scalars(select(User).where(User.status == "active")).all()
     df_saldi = pd.DataFrame().from_records(
@@ -119,15 +92,58 @@ with conn.session as session:
             for user in users
         ]
     )
-    st.dataframe(
-        df_saldi,
-        column_config={"Saldo": st.column_config.NumberColumn(format="€ %g")},
-    )
-    summe = df_saldi["Saldo"].sum()
-    # calculate the sum of all positive saldos
-    summe_positiv = df_saldi[df_saldi["Saldo"] > 0]["Saldo"].sum()
-    st.metric("Summe aller Saldi", "€ " + str(summe).replace(".", ","))
+summe_positiv = df_saldi[df_saldi["Saldo"] > 0]["Saldo"].sum()
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.metric("Einzahlungen", "€ " + str(einzahlungen).replace(".", ","))
+    st.metric("Auszahlungen", "€ " + str(auszahlungen).replace(".", ","))
+    st.metric("Korrekturen", "€ " + str(korrekturen).replace(".", ","))
+    st.metric("Kassenstand", "€ " + str(einzahlungen - auszahlungen + korrekturen).replace(".", ","))
+    st.metric("offene Rechnungen", "€ " + str(summe).replace(".", ","))
+    
+    st.metric("Einkäufe", "€ " + str(einkaeufe).replace(".", ","))
+
+with col2:
+
+    st.metric("Kaffeeanzahl Mitglieder", str(kaffeeanzahl_mitglieder))
+    st.metric("Kaffeeanzahl Gäste", str(kaffeeanzahl_gaeste))
+    st.metric("Kaffeeanzahl gesamt", str(kaffeeanzahl_mitglieder + kaffeeanzahl_gaeste))
     st.metric("Summe der Guthaben", "€ " + str(summe_positiv).replace(".", ","))
-    st.write(
-        "Ein positiver Saldo bedeutet, dass die Person Guthaben hat. Negativer Saldo bedeutet, dass Rechnungen offen sind. Kaffees des laufenden Monats für den noch keine Abrechnung gemacht wurde sind im individuellen Saldo nicht enthalten."
-    )
+    st.metric("Überschuss", "€ " + str(mitgliedskosten + gastkosten + einkaeufe).replace(".", ","))
+    
+with col3:
+    st.metric("Kaffeeumsatz Mitglieder", "€ " + str(mitgliedskosten).replace(".", ","))
+    st.metric("Kaffeeumsatz Gäste", "€ " + str(gastkosten).replace(".", ","))
+    st.metric("Kaffeeumsatz gesamt", "€ " + str(mitgliedskosten + gastkosten).replace(".", ","))
+
+st.dataframe(
+    df_zahlungen,
+    column_config={
+        "Betrag": st.column_config.NumberColumn(format="€ %g"),
+        "Datum": st.column_config.DatetimeColumn(format="DD.MM.YY"),
+    },
+)
+
+st.subheader("offene Rechnungen")
+
+st.dataframe(
+    offene_rechnungen,
+    column_config={
+        "Betrag": st.column_config.NumberColumn(format="€ %g"),
+        "Datum": st.column_config.DatetimeColumn(format="DD.MM.YY"),
+    },
+)
+
+
+st.subheader("Saldi der Nutzenden")
+
+st.dataframe(
+    df_saldi,
+    column_config={"Saldo": st.column_config.NumberColumn(format="€ %g")},
+)
+
+st.write(
+    "Ein positiver Saldo bedeutet, dass die Person Guthaben hat. Negativer Saldo bedeutet, dass Rechnungen offen sind. Kaffees des laufenden Monats für den noch keine Abrechnung gemacht wurde sind im individuellen Saldo nicht enthalten."
+)
